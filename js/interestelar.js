2d3051d26af199add40cd094f260df00f2cf9484
Juan Manoel <gmjm2004@gmail.com>
2025-05-12 14:15:00 -0500

    generating mapa

--- js/interestelar.js
+++ js/interestelar.js
@@ -1,3 +1,98 @@
+const validaciones = (data) => {
+  // Validaciones de los datos del formulario
+  const { dimensionesX, dimensionesY, inicioX, inicioY, finalX, finalY, agujerosNegros, estrellasGigantes, agujerosGusano, celdasEnergia } = data;
+  if (dimensionesX<30 || dimensionesY<30){
+    alert("Las dimensiones del mapa deben ser mayores a 30");
+    return false;
+  }
+  if (((inicioX < 0 || inicioY < 0) || (finalX < 0 || finalY < 0)) || 
+      ((inicioX > dimensionesX || inicioY > dimensionesY) || (finalX > dimensionesX || finalY > dimensionesY))) {
+    alert("Las coordenadas de inicio y fin deben estar dentro del mapa");
+    return false;
+  }
+  if (inicioX == finalX && inicioY == finalY) {
+    alert("Las coordenadas de inicio y fin no pueden ser iguales");
+    return false;
+  }
+  if (agujerosNegros < 0 || agujerosNegros > (dimensionesX * dimensionesY) / 2) {
+    alert("El número de agujeros en el mapa no puede ser menor a 0 o mayor a la mitad del mapa");
+    return false;
+  }
+  if (estrellasGigantes < 0 || estrellasGigantes > agujerosNegros) {
+    alert("El número de estrellas gigantes en el mapa no puede ser menor a 0 o mayor que la cantidad de agujeros negros");
+    return false;
+  }
+  if (agujerosGusano < 0 || agujerosGusano > (dimensionesX * dimensionesY) / 3) {
+    alert("El número de agujeros de gusano en el mapa no puede ser menor a 0 o mayor a un tercio del mapa");
+    return false;
+  }
+
+  if (celdasEnergia < 0 || celdasEnergia > (dimensionesX * dimensionesY) / 10) {
+    alert("El número de celdas de energía en el mapa no puede ser menor a 0 o mayor a un décimo del mapa");
+    return false;
+  }
+  return true;
+}
+
+
+const auxliar = (data,din,arr)=>{
+  for (let i = 0; i < din; i++) {
+    let x = Math.floor(Math.random() * data.dimensionesX);
+    let y = Math.floor(Math.random() * data.dimensionesY);
+    if((x==data.inicioX && y==data.inicioY) || (x==data.finalX && y==data.finalY)){
+      i--;
+      continue;
+    }
+    arr[i] = [x,y];
+  }
+
+  return arr
+}
+
+
+const generarMapa = (data) =>{
+  let mapa = [[]];
+  let agujerosNetros = [[]];
+  let estrellasGigantes = [[]];
+  let agujerosGusano = [];
+  let celdasEnergia = [[]];
+  for (let i = 0; i < data.dimensionesX; i++) {
+    mapa[i] = [];
+    for (let j = 0; j < data.dimensionesY; j++) {
+      mapa[i][j] = Math.floor(Math.random() * 11);;
+    }
+  }
+  mapa[data.inicioX][data.inicioY] = 0;
+  mapa[data.finalX][data.finalY] = 0;
+  
+  agujerosNetros = auxliar(data,data.agujerosNegros,agujerosNetros)
+  estrellasGigantes = auxliar(data,data.estrellasGigantes,estrellasGigantes)
+  celdasEnergia = auxliar(data,data.celdasEnergia,celdasEnergia)
+
+    for (let i = 0; i < data.agujerosGusano; i++) {
+    let x_inicio = Math.floor(Math.random() * data.dimensionesX);
+    let y_inicio = Math.floor(Math.random() * data.dimensionesY);
+    let x_fin = Math.floor(Math.random() * data.dimensionesX);
+    let y_fin = Math.floor(Math.random() * data.dimensionesY);
+    if((x_inicio==data.inicioX && y_inicio==data.inicioY) || (x_inicio==data.finalX && y_inicio==data.finalY)){
+      i--;
+      continue;
+    }
+    agujerosGusano[i] = {inicio: [x_inicio, y_inicio], fin: [x_fin, y_fin]};
+  }
+
+  let backtraking = {
+    mapa: mapa,
+    agujerosNetros: agujerosNetros,
+    estrellasGigantes: estrellasGigantes,
+    agujerosGusano: agujerosGusano,
+    celdasEnergia: celdasEnergia,
+  }
+
+  return backtraking
+}
+
+
 const loadDataForm = () => {
   // Captura los datos del formulario
   const dimensionesX = document.getElementById("dimensionesX").value;
@@ -24,11 +119,15 @@ const loadDataForm = () => {
     celdasEnergia: parseInt(celdasEnergia),
   };
 
-  // Muestra los datos en la consola (puedes procesarlos como desees)
-  console.log("Datos del formulario:", inputMapa);
-
-  // Aquí puedes agregar lógica adicional para procesar los datos
-  alert("Datos cargados correctamente. Revisa la consola para más detalles.");
+  if(validaciones(inputMapa)){
+    // Si las validaciones son correctas, genera el mapa
+    let mapa = generarMapa(inputMapa);
+    const path = './data/mapa.json';
+    localStorage.setItem(path, JSON.stringify(mapa));
+    const dataSalva = localStorage.getItem(path);
+    console.log(dataSalva);
+    
+  }
 };
 
 export const interestelar = () => {
