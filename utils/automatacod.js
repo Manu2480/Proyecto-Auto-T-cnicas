4f6c76fc122d97b5f4ac6f25ccc750776a2ffeda
Juan Manoel <gmjm2004@gmail.com>
2025-06-06 17:10:33 -0500

    autoamata codigo universidad esta Ok

---
+++ utils/automatacod.js
@@ -0,0 +1,79 @@
+export function validarCodigo(cod){
+    // # Numero, A Letra, - Guion
+    let estado = "Q0"; // estado oficial
+    let estadoAux = "Q0"; // estado auxiliar para los errores
+    const matrix = {
+        "Q0": {"#":"Q2","A":"Q1","-":"Q1"},
+        "Q1":{"#":"Q1","A":"Q1","-":"Q1"},
+        "Q2":{"#":"Q3","A":"Q1","-":"Q1"},
+        "Q3":{"#":"Q4","A":"Q1","-":"Q1"},
+        "Q4":{"#":"Q5","A":"Q1","-":"Q1"},
+        "Q5":{"#":"Q1","A":"Q1","-":"Q6"},
+        "Q6":{"#":"Q1","A":"Q7","-":"Q1"},
+        "Q7":{"#":"Q1","A":"Q8","-":"Q1"},
+        "Q8":{"#":"Q1","A":"Q1","-":"Q9"},
+        "Q9":{"#":"Q10","A":"Q1","-":"Q1"},
+        "Q10":{"#":"Q11","A":"Q1","-":"Q1"},
+        "Q11":{"#":"Q12","A":"Q1","-":"Q1"},
+        "Q12":{"#":"Q13","A":"Q1","-":"Q1"},
+        "Q13":{"#":"Q1","A":"Q1","-":"Q1"},
+
+    };
+    if (cod.length === 0) {
+        return {estado: false, message: "El código no puede estar vacío"};
+    }
+    for (let i = 0; i < cod.length; i++) {
+        const simbolo = cod[i];
+        
+        
+        if(esLetra(simbolo)){
+            if(!matrix[estado] || !matrix[estado]["A"]){
+                return {estado: false, message: `Transición no válida en estado ${estado} con símbolo ${simbolo}`};
+            }
+            estadoAux = matrix[estado]["A"];
+        }else if(esNumero(simbolo)){
+            if(!matrix[estado] || !matrix[estado]["#"]){
+                return {estado: false, message: `Transición no válida en estado ${estado} con símbolo ${simbolo}`};
+            }
+            // Validar el año (19xx - xxxx)
+            if (estado === "Q0" && simbolo === '0') {
+                return {estado: false, message: `Año no válido en estado ${estado} con símbolo ${simbolo}`};
+            }
+            if (estado === "Q2") {
+                let aux = cod[i-1];                
+                if (aux === '1' && simbolo != '9') {
+                    return {estado: false, message: `Año no válido en estado ${estado} con símbolo ${simbolo}`};
+                }
+            }
+
+            estadoAux = matrix[estado]["#"];
+        }else if(simbolo === '-'){
+            if(!matrix[estado] || !matrix[estado]["-"]){
+                return {estado: false, message: `Transición no válida en estado ${estado} con símbolo ${simbolo}`};
+            }
+            estadoAux = matrix[estado]["-"];
+        }else{
+            return {estado: false, message: `Símbolo no válido: ${simbolo}`};
+        }
+        if (estadoAux === "Q1") {
+            return {estado: false, message: `Transición no válida en estado ${estado} con símbolo ${simbolo}`};
+        }
+        estado = estadoAux;
+    }
+
+    const mensaje = {
+        estado: estado === "Q13",
+        message: estado === "Q13" ? `Código válido, estado final: ${estado}` : `Código no válido, estado final: ${estado}`
+    }
+    return mensaje;
+}
+
+function esNumero(simbolo) {
+    return simbolo >= '0' && simbolo <= '9';
+}
+
+function esLetra(simbolo) {
+    return (simbolo >= 'A' && simbolo <= 'Z') || (simbolo >= 'a' && simbolo <= 'z');
+}
+
+console.log(validarCodigo("1923-cd-0484")); // Ejemplo de uso
